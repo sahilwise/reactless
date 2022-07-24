f8e716dd434052c078cfe3981da14a2c2e71b9e6
sahilnetic <jsthil99@gmail.com>
2022-07-25 01:16:01 +0530

    done

--- pages/components/Components.js
+++ pages/components/Components.js
@@ -1,7 +1,7 @@
 const Components = ()=>{
   return(
     <>
-      <div className="grid grid-cols-3 px-8 bg-transparent space-x-8">
+      <div className="grid sm:grid-cols-3 px-8 bg-transparent gap-y-8 sm:space-x-8">
           
       <div className="bg-whit3/30 px-3 py-3 rounded-lg shadow-lg shadow-lg opacity-90">
             <p className="py-20" />
@@ -19,9 +19,7 @@ const Components = ()=>{
             <p className="py-20" />
             <h3>Youtube Page</h3>
             <p className="text-xs">@sahilnetic</p>
-          </div>
-
-          
+          </div>        
 
           
 
