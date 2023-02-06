8ed402e94da0907506197672cd3ebf3be118d100
sahilnetic <jsthil99@gmail.com>
2023-02-07 00:03:53 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -31,10 +31,10 @@ const Beforelogin = () => {
             <h1
               className="text-3xl font-extrabold leading-10 tracking-tight text-left text-black text-center sm:leading-none md:text-6xl text-3xl lg:text-6xl"
             >
-              <span className="inline md:block">Open Source Components</span>
+              <span className="inline md:block">Open Source React &</span>
               <span
                 className="relative mt-2 bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-teal-500 md:inline-block"
-              > & <span className="bg-clip-text text-transparent bg-gradient-to-r from-zinc-500 via-cyon-500 to-teal-500"> Templates </span> Using React Native</span>
+              > React <span className="bg-clip-text text-transparent bg-gradient-to-r from-zinc-500 via-cyon-500 to-teal-500"> Native </span> Components</span>
             </h1>
           </div>
   
@@ -44,7 +44,7 @@ const Beforelogin = () => {
   <a
       className="flex hover:border-2 hover:shadow-xl items-center text-white border border-2 justify-center w-full sm:px-10 mx-16 py-4 leading-6 bg-black rounded-lg font-black"
     >
-    &nbsp;&nbsp;<Image width={20} height={20} src="/android.png" />&nbsp;&nbsp; Get Started
+    &nbsp;&nbsp;<Image width={20} height={20} src="/github.png" />&nbsp;&nbsp; Fork On Github
     </a>
     </Link>
   </span>
@@ -53,8 +53,8 @@ const Beforelogin = () => {
 
 
   <div className="flex space-x-5 max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8">
-    <h3 className="underline decoration-2 underline-offset-8">Components</h3>
-    <h3>Templates</h3>
+    <h3 className="underline decoration-2 underline-offset-8">React js</h3>
+    <h3>React Native</h3>
     <h4>Web View</h4>
   </div>
   {/* Components */}
@@ -79,15 +79,6 @@ const Beforelogin = () => {
     </div>
   </div>
 </footer>                           
-  
-
-
-  
-  
-          {/* <aside
-            className="fixed flex items-center justify-center text-white rounded-full bottom-20 right-10"
-          >
-          </aside> */}
   
         </Layout>
 
